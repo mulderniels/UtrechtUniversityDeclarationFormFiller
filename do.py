6f35138612c15ef7b044bf37b0e46ab3e5053e50
mulderniels <ik@mulderniels.nl>
2016-04-30 21:34:08 +0200

    init
    init

---
+++ do.py
@@ -0,0 +1,38 @@
+#made by n.mulder1@uu.nl 2016
+
+#fills Utrecht University declaration form C1 (template.xlsx) for each row in data.xlsx
+
+#init
+import os.path
+from openpyxl import load_workbook
+
+#do stuff
+print('hallo')
+
+data = load_workbook(filename = 'data.xlsx')
+data = data.active #select the active sheet
+
+personsN = data.max_row-1 #no of scenarios
+
+templateFile = load_workbook(filename = 'template.xlsx')
+template = templateFile.active #select the active sheet
+
+#loop trough persons
+for iPerson in range(2, personsN+1+1):
+	templateFile = load_workbook(filename = 'template.xlsx')
+	result = templateFile.active #select the active sheet
+	
+	achternaam = str(data.cell(row = iPerson, column = 3).value);
+	filename = achternaam + ' ' + str(iPerson);
+	
+	result['A11'] = str(data.cell(row = iPerson, column = 2).value)[0] #voorletter
+	result['J11'] = achternaam #achternaam
+	result['A13'] = str(data.cell(row = iPerson, column = 5).value) #straat
+	result['J13'] = str(data.cell(row = iPerson, column = 6).value) #postcode
+	result['A15'] = str(data.cell(row = iPerson, column = 3).value) #iban
+	result['A17'] = str(data.cell(row = iPerson, column = 4).value) #swift
+	if not os.path.exists('output/' + filename):
+	    os.makedirs('output/' + filename)
+	templateFile.save('output/' + filename + '/declaratie ' + filename + '.xlsx')
+
+print('doei')
---
+++ readme.md
@@ -0,0 +1,5 @@
+# Utrecht University Declaration Form Filler
+
+fills Utrecht University declaration form C1 (template.xlsx) for each row in data.xlsx
+
+made by n.mulder1@uu.nl 2016
